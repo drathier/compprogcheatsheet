6eaa90705480cc3414c747166f3a17a34f1c5872
drathier <fille.haglund@hotmail.com>
2015-10-24 01:13:32 +0200

    minimized binary search source

--- code/binary-search/binary-search.cpp
+++ code/binary-search/binary-search.cpp
@@ -1,26 +1,12 @@
-#include <iostream>
-using namespace std;
-
-int tst[] = {1, 1, 2, 3, 3, 4, 5, 5, 6, 7};
-
-bool lte(int a) {
-	return tst[a] <= 0;
-}
-
-int search(int n) {
+int search(int n, function<bool(int)> lt) {
 	int i = 0, k = n;
 	while (i < k) {
 		int m = i + (k - i) / 2;
-		cout << "i=" << i << ", m=" << m << ", k=" << k << endl;
-		if (lte(m)) { // max(lte(m)) s.t. lte(m) is true
+		if (lt(m)) { // max(lt(m)) s.t. lt(m) is true
 			i = m + 1;
 		} else {
 			k = m;
 		}
 	}
 	return i; // returns where needle SHOULD be!
-}
-
-int main() {
-	cout << search(10) << endl;
 }
