8fec33f60eb1de9a7deb2a286dad4dfea6da3b93
Henke Adolfsson <henad221@student.liu.se>
2015-10-24 08:29:46 +0100

    lambda example is now compileable

--- code/c++11/lambdas.cpp
+++ code/c++11/lambdas.cpp
@@ -1,12 +1,11 @@
-#include <function>
+#include <functional>
 #include <iostream>
-
 using namespace std;
-
 int main(){
-	function<void(int, int)> prnt = [](int a, int b){cout << a << " and " << b << endl;}
+	function<void(int, int)> prnt = [](int a, int b){cout << a << " and " << b << endl;};
 	int the_int = 42;
-	function<void(int)> prnt2 = [&the_int](int a){cout << a << " THE_INT: " << the_int << endl;}
+	function<void(int)> prnt2 = [&the_int](int a){cout << a << " THE_INT: " << the_int << endl;};
 	int leet = 1337;
-	function<void()> prnt3 = [&](){cout << the_int << " vs. " << leet << endl;}
+	function<void()> prnt3 = [&](){cout << the_int << " vs. " << leet << endl;};
+	function<void()> prnt4 = [=](){cout << "Captured by value, not reference" << the_int << leet << endl;};
 }
