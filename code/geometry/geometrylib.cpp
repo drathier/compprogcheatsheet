6aefdd3bd3cae8008c0f11f78059f9c3dd71f843
CatEars <CatEars13@gmail.com>
2015-11-02 12:53:25 +0100

    Update geometrylib.cpp
    Temporary change the geometrylib so that we can.... be inspired by other code

--- code/geometry/geometrylib.cpp
+++ code/geometry/geometrylib.cpp
@@ -1,11 +1,40 @@
-#include <iostream>
 #include <cmath>
 using namespace std;
 const double EPS = 0.0000001;
 const double TAU = 6.28318530717958647692528;
 const double PI = TAU/2.0;
 using T = int;
+/* 
+template <class T>
+struct Point {
+	typedef T coordType;
+	typedef Point P;
+	typedef const P & R;
+	T x, y;
+	explicit Point(T x=T(), T y=T()) : x( x), y( y) { }
+	bool operator<(R p) const { return x<p.x || x==p.x && y<p.y; }
+	bool operator==(R p) const { return x==p.x && y==p.y; }
+	P operator+(R p) const { return P(x+p.x, y+p.y); }
+	P operator-(R p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(R p) const { return x*p.x + y*p.y; }
+	T cross(R p) const { return x*p.y - y*p.x; }
+	T dist2() const { return x*x + y*y; } //distance^2
+	double dist() const { return sqrt((double)dist2()); } //length
+	//angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/(T)dist(); } //makes dist()=1
+	P perp() const { return P(-y, x); } //rotates +pi/2 radians
+	P normal() const { return perp().unit(); }
+	//returns point rotated a radians counterclockwise around origo
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a));
+	}
+};
 
+
+*/
 struct Point {
 	T x, y;
 	Point() {}
